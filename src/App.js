379aed056357c3393e93b3ac2cfbfe94c9ca730f
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-09-15 16:00:08 -0300

    imports e exports feitos

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navbar from './NavBar'
+import Corpo from './Corpo'
+
+export default function App(){
+    return(
+    <div>
+     <Navbar/>
+     <Corpo/>
+
+    <div class="fundo-mobile">
+      <ion-icon name="home"></ion-icon>
+      <ion-icon name="search-outline"></ion-icon>
+      <ion-icon name="add-circle-outline"></ion-icon>
+      <ion-icon name="heart-outline"></ion-icon>
+      <ion-icon name="person-outline"></ion-icon>
+    </div>
+</div>
+    )
+}
